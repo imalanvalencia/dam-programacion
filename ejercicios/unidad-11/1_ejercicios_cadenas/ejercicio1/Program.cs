1b19fd83c746d795f412aca105f302a73b50d6bd
Alan Valencia <alavalizq@alu.edu.gva.es>
2025-11-28 13:43:03 +0100

    feat: adelantando un poco el ejercicio1

--- ejercicios/unidad-11/1_ejercicios_cadenas/ejercicio1/Program.cs
+++ ejercicios/unidad-11/1_ejercicios_cadenas/ejercicio1/Program.cs
@@ -5,6 +5,23 @@ namespace ejercicio1
     public class Program
     {
         //TODO: Implementar los métodos necesarios
+
+        public bool SoloNumeros(string contraseña)
+        {
+            foreach (var character in contraseña.ToCharArray())
+            {
+                (int)character;
+            }
+
+            return ;
+        }
+
+        public bool SoloLetras(string contraseña)
+        {
+            return contraseña == "a";
+        }
+
+        
         static void Main(string[] args)
         {
             //TODO: Implementar la lógica necesaria. Fíjate en la salida por pantalla.
