bbad357b578e278e4b53f19071c2c85212454a3f
Alan Valencia <alavalizq@alu.edu.gva.es>
2025-12-02 12:50:02 +0100

    feat: Terminando ejercicio 2

--- ejercicios/unidad-11/1_ejercicios_cadenas/ejercicio2/Program.cs
+++ ejercicios/unidad-11/1_ejercicios_cadenas/ejercicio2/Program.cs
@@ -5,9 +5,35 @@ namespace ejercicio2
     public class Program
     {
         //TODO: Implementar los métodos necesarios
+        public static string FormateaString(string frase) => string.Join("", frase.ToLower().Split(' '));
+
+        public static string Reverse(string frase)
+        {
+            string revereString = "";
+
+            foreach (var character in frase)
+            {
+                revereString = character + revereString;
+            }
+
+            return revereString;
+        }
+
+        public static bool EsPalindroma(string frase) => FormateaString(frase) == FormateaString(Reverse(frase));
+
         static void Main(string[] args)
         {
             //TODO: Implementar la lógica necesaria. Fíjate en la salida por pantalla.
+
+            Console.Write("Introduce una frase: ");
+            string frase = Console.ReadLine() ?? "";
+
+            Console.WriteLine(EsPalindroma(frase) ? "Es palíndroma" : "No es palíndroma");
+
+
+
+            Console.WriteLine("Presiona cualquier tecla para salir...");
+            Console.ReadLine();
         }
 
     }
