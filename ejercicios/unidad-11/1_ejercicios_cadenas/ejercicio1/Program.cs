a0ece2ea46e01f1163e2d5546b23829de4999217
Alan Valencia <alavalizq@alu.edu.gva.es>
2025-12-03 10:58:52 +0100

    feat: Completando ejercicio 1

--- ejercicios/unidad-11/1_ejercicios_cadenas/ejercicio1/Program.cs
+++ ejercicios/unidad-11/1_ejercicios_cadenas/ejercicio1/Program.cs
@@ -6,27 +6,66 @@ namespace ejercicio1
     {
         //TODO: Implementar los métodos necesarios
 
-        public bool SoloNumeros(string contraseña)
+
+
+        public static bool SoloNumeros(string contraseña) => int.TryParse(contraseña, out _);
+
+        public static bool SoloLetras(string contraseña)
         {
-            foreach (var character in contraseña.ToCharArray())
+            foreach (var character in contraseña)
             {
-                (int)character;
+                if (!(character is >= 'A' and <= 'Z' or >= 'a' and <= 'z')) return false;
             }
-
-            return ;
+            return true;
         }
 
-        public bool SoloLetras(string contraseña)
+        // CREA METODOS ContieneLetras, ContieneNumeros y ContieneEspeciales 
+        public static bool ContieneLetras(string contraseña)
         {
-            return contraseña == "a";
+            foreach (var character in contraseña)
+            {
+                if (character is >= 'A' and <= 'Z' or >= 'a' and <= 'z') return true;
+            }
+            return false;
         }
 
-        
+
+        public static bool ContieneNumeros(string contraseña)
+        {
+            foreach (var character in contraseña)
+            {
+                if (character is >= '0' and <= '9') return true;
+            }
+            return false;
+        }
+        public static bool ContieneEspeciales(string contraseña)
+        {
+            foreach (var character in contraseña)
+            {
+                if (!(character is >= 'A' and <= 'Z' or >= 'a' and <= 'z' or >= '0' and <= '9')) return true;
+            }
+            return false;
+        }
+
+        public static string NivelSeguridad(string contraseña) => contraseña switch
+        {
+            _ when SoloNumeros(contraseña) || contraseña.Length < 8 => "Muy débil",
+            _ when ContieneLetras(contraseña) && ContieneNumeros(contraseña) && ContieneEspeciales(contraseña) => "Muy fuerte",
+            _ when ContieneLetras(contraseña) && ContieneNumeros(contraseña) => "Fuerte",
+            _ when SoloLetras(contraseña) => "Débil",
+            _ => "Ultra fuerte?"
+        };
+
         static void Main(string[] args)
         {
             //TODO: Implementar la lógica necesaria. Fíjate en la salida por pantalla.
+            Console.Write("Introduce una contraseña: ");
+            string contraseña = Console.ReadLine() ?? "";
+            Console.WriteLine($"Nivel de seguridad: {NivelSeguridad(contraseña)}");
+
+            Console.ReadLine();
         }
 
     }
-      
+
 }
