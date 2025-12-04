8ca4e64ce97beaef5ef1a4653737ff2622fa823a
Alan Valencia <alavalizq@alu.edu.gva.es>
2025-12-04 12:59:16 +0100

    feat: Completando ejercicio 1

--- ejercicios/unidad-11/2_ejercicios_regexp/ejercicio1/Program.cs
+++ ejercicios/unidad-11/2_ejercicios_regexp/ejercicio1/Program.cs
@@ -3,9 +3,24 @@ using System.Text.RegularExpressions;
 
 public class Program
 {
-   
+
+    public static string[] ExtraerEtiquetas(string input)
+    {
+        var matches = Regex.Matches(input, @"<(?<tag>\w+)>(?<content>.*?)</\k<tag>>", RegexOptions.Singleline);
+        var results = new string[matches.Count];
+        for (int i = 0; i < matches.Count; i++)
+        {
+            results[i] = matches[i].Groups["content"].Value;
+        }
+        return results;
+    }
+
     static void Main()
     {
-      
+        string tag = "<p>Hola mundo</p><p>¿Qué tal estás?</p>";
+
+        Console.WriteLine("ExtraeEtiquetas(\"" + tag + "\")");
+        Console.WriteLine("Devuelve: " + string.Join(", ", ExtraerEtiquetas(tag)));
+        Console.ReadLine();
     }
 }
