f4c42a037e40cb51a0e66c276c50c2dc1be5c6fe
Alan Valencia <alavalizq@alu.edu.gva.es>
2025-12-12 09:29:46 +0100

    feat: Terminando ejercicios unidad 11

--- ejercicios/unidad-11/2_ejercicios_er/ejercicio4/Program.cs
+++ ejercicios/unidad-11/2_ejercicios_er/ejercicio4/Program.cs
@@ -3,17 +3,30 @@ using System.Text.RegularExpressions;
 
 public class Program
 {
-   //TODO: Completa el código necesarios para cumplir con las expecificaciones del ejercicio
+    //TODO: Completa el código necesarios para cumplir con las expecificaciones del ejercicio
+
+    public static string[] ExtraerEtiquetas(string entrada)
+    {
+        MatchCollection matchea = Regex.Matches(entrada, @"<(?<tag>\w+)>(?<content>.*?)</\k<tag>>", RegexOptions.Singleline);
+        var resultado = new string[matchea.Count];
+
+        for (int i = 0; i < matchea.Count; i++)
+        {
+            resultado[i] = matchea[i].Groups["content"].Value;
+        }
+        
+        return resultado;
+    }
 
     static void Main()
     {
         Console.WriteLine("Ejercicio 4: Extracción de contenido de etiquetas HTML\n");
-        
+
         Console.Write("Introduce la línea HTML: ");
         string entrada = Console.ReadLine() ?? "";
-        
+
         string[] etiquetas = ExtraerEtiquetas(entrada);
-        
+
         Console.Write("El contenido de la línea es: [");
         for (int i = 0; i < etiquetas.Length; i++)
         {
@@ -21,7 +34,7 @@ public class Program
             if (i < etiquetas.Length - 1) Console.Write(", ");
         }
         Console.WriteLine("]");
-        
+
         Console.WriteLine("\nPresiona Enter para salir...");
         Console.ReadLine();
     }
--- ejercicios/unidad-11/2_ejercicios_er/ejercicio5/Program.cs
+++ ejercicios/unidad-11/2_ejercicios_er/ejercicio5/Program.cs
@@ -4,12 +4,37 @@ using System.Text.RegularExpressions;
 
 public class Program
 {
-    public static string ReescribeTiko(string frase)
-    {
-        //TODO: Completa el código necesarios para cumplir con las expecificaciones del ejercicio
-        return frase;
-    }
 
+    static string NumberToString(Match m) => m.Value switch 
+    {
+        "1" => "uno",
+        "2" => "dos",
+        "3" => "tres",
+        "4" => "cuatro",
+        "5" => "cinco",
+        "6" => "seis",
+        "7" => "siete",
+        "8" => "ocho",
+        "9" => "nueve",
+        _ => "cero"
+    };
+
+
+
+public static string ReescribeTiko(string frase)
+{
+    frase = Regex.Replace(frase, @"(j[aeiou]){2,}", "😂", RegexOptions.IgnoreCase);
+
+    frase = Regex.Replace(frase, @"\bx q\b", "por qué", RegexOptions.IgnoreCase);
+
+    frase = Regex.Replace(frase, @"([a-km-qs-z])\1+", "$1", RegexOptions.IgnoreCase);
+    frase = Regex.Replace(frase, @"([rl])\1{2,}", "$1$1", RegexOptions.IgnoreCase);
+
+    frase = Regex.Replace(frase, @"\d", NumberToString);
+    
+    
+    return frase;
+}
     static void Main()
     {
         Console.OutputEncoding = Encoding.UTF8;
