711f7043d05e57db7e50799fad543afcd0d91298
Alan Valencia <alavalizq@alu.edu.gva.es>
2026-03-26 12:45:49 +0100

    feat: añadiendo ejercicios de ejemplo de examen

---
+++ teoria/examen-3/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace Ejercicio1
+{
+    class Program
+    {
+        static void Main()
+        {
+            // Muestra el número del clientes con 45 años o más
+            Console.WriteLine(
+               
+            );
+
+            // Paises con los nacidos después de 1999 (Sin repeticiones).
+            Console.WriteLine(
+                
+            );
+
+            // Muestra Nombre Apellido y Pais, ordenados por nombre, de aquellos clientes que sean de Germany, Spain y Italy.
+            Console.WriteLine(
+                           );
+
+            // Muetra Paises y número de clientes de aquellos paises en que tenga 20 o más clientes.
+            Console.WriteLine(
+                           );
+        }
+    }
+}
+
